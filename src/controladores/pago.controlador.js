d1ebf6980921ca448aead8679c8e405dfc86676a
Fabian <fabianbellov@gmail.com>
2024-05-24 14:00:34 -0400

    Añadido método: obtener estado del POS

--- src/controladores/pago.controlador.js
+++ src/controladores/pago.controlador.js
@@ -1,11 +1,70 @@
 import { PagoModelo } from "../modelos/pago.modelo.js";
 
+// Obtener Estatus 
 const obtenerStatus = async (req, res) => {
+    let estado = "No inicializado";
+    console.log("ESTADO: ", estado);
     try {
-        const result = await PagoModelo.obtenerStatus();
-        res.json(result);
+        estado = "Inicializando";
+        console.log("ESTADO: ", estado);
+        const port = await PagoModelo.conectarPos();
+        estado = "reconociendo puertos";
+        console.log("ESTADO: ", estado);
+        if (!port) {
+            estado = "No se encontraron puertos";
+            console.log("ESTADO: ", estado);
+            return res.status(500).json({ message: 'No se encontraron puertos' });
+        }
+        else {
+            estado = "Si se encontraron puertos";
+            console.log("ESTADO: ", estado);
+            estado = "Intentando desconectar";
+            console.log("ESTADO: ", estado);
+            const desconectado = await PagoModelo.desconectarPos();
+            if (desconectado) {
+                estado = "POS desconectado";
+                console.log("ESTADO: ", estado);
+                res.status(200).json({ message: 'Se ha conectado y desconectado el POS correctamente' });
 
-    } catch (error) { res.status(500).json({ message: error.message }); }
+            } else {
+                res.status(200).json({ message: 'Solo se ha conseguido conectar al POS' });
+            }
+        }
+
+    } catch (error) {
+        console.log('ERROR:', error.message);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
 };
 
-export const PagoControlador = { obtenerStatus, };
+// Obtener ultima venta 
+const obtenerUltimaVenta = async (req, res) => {
+    let estado = "No inicializado";
+    console.log("ESTADO: ", estado);
+    try {
+        estado = "Inicializando e intentando obtener utlima venta";
+        console.log("ESTADO: ", estado);
+        let ultimaVenta = await PagoModelo.obtenerUltimaVenta();
+        estado = "obteniendoo ultima venta";
+        console.log("ESTADO: "+ estado );
+        console.log("ULTIMA VENTA: "+ ultimaVenta );
+        if (!ultimaVenta) {
+            estado = "No se encontraron puertos";
+            console.log("ESTADO: ", estado);
+            return res.status(500).json({ message: 'No se encontraron puertos' });
+        }
+        else {
+            
+        }
+
+    } catch (error) {
+        console.log('ERROR:', error.message);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+};
+
+
+
+
+
+export const PagoControlador = { obtenerStatus };
--- src/modelos/pago.modelo.js
+++ src/modelos/pago.modelo.js
@@ -1,29 +1,54 @@
+import pkg from 'transbank-pos-sdk';
 
-const obtenerStatus = async () => {
+// Incializando sdk POS de TBK
+const { POSAutoservicio } = pkg;
+const pos = new POSAutoservicio();
 
-    // Habilitado sistema debug
-    pos.setDebug(true);
+// Depuración activada
+pos.setDebug(true);
 
-    // Intento de conexión al POS
-    pos.autoconnect()
-        .then((port) => {
-            if (port === false) {
-                return { "Message: ": "No se encontraron puertos" }
-                console.log('No se encontró ningún POS conectado');
-            }
-            console.log('Connected to PORT: ', port.path)
-            return { "Message: ": "Se ha conectado con éxtio" }
-            pos.loadKeys() // Cargar llaves
-        })
-        .catch((err) => {
-            console.log('Ocurrió un error inesperado', err);
-            return {
-                "Message: ": "Error inesperado",
-                "Error": err
-            }
-        })
-}
-
-export const PagoModelo = {
-    obtenerStatus
+// Conectar POS
+const conectarPos = async () => {
+    try {
+        const port = await pos.autoconnect();
+        if (!port) {
+            console.log('No se encontró ningún POS conectado');
+            return null;
+        }
+        /* console.log('Connected to PORT:', port.path); */
+        return port;
+    } catch (error) {
+        console.log('Ocurrió un error inesperado:', error);
+        throw error;
+    }
 };
+
+// Desconectar POS
+const desconectarPos = async () => {
+    try {
+        await pos.disconnect();
+        return true;
+    } catch (error) {
+        console.log('Ocurrió un error inesperado:', error);
+        throw error;
+        return false;
+    }
+};
+
+// Ultima venta
+const obtenerUltimaVenta = async () => {
+    try {
+        pos.getLastSale().then((response) => {
+            console.log('getLastSale ejecutado. Respuesta: ', response);
+            return response;
+        }).catch((err) => {
+            console.log('Ocurrió un error inesperado', err);
+        });
+    } catch (error) {
+        console.log('Ocurrió un error inesperado:', error);
+        throw error;
+        return false;
+    }
+};
+
+export const PagoModelo = { conectarPos, desconectarPos };
