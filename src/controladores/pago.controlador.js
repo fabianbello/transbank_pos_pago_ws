924f4035987199a0309c5068df24aaf49ecdc359
Fabian <fabianbellov@gmail.com>
2024-05-27 09:22:40 -0400

    Añadido función obtener última venta
    Método para obtener la información de la ultima venta realizada en el POS, en caso de no existir venta, se notifica que no existe venta.

--- src/controladores/pago.controlador.js
+++ src/controladores/pago.controlador.js
@@ -48,25 +48,13 @@ const obtenerUltimaVenta = async (req, res) => {
         estado = "obteniendoo ultima venta";
         console.log("ESTADO: "+ estado );
         console.log("ULTIMA VENTA: "+ ultimaVenta );
-        if (!port) {
+        if (!ultimaVenta) {
             estado = "No se encontraron puertos";
             console.log("ESTADO: ", estado);
             return res.status(500).json({ message: 'No se encontraron puertos' });
         }
         else {
-            estado = "Si se encontraron puertos";
-            console.log("ESTADO: ", estado);
-            estado = "Intentando desconectar";
-            console.log("ESTADO: ", estado);
-            const desconectado = await PagoModelo.desconectarPos();
-            if (desconectado) {
-                estado = "POS desconectado";
-                console.log("ESTADO: ", estado);
-                res.status(200).json({ message: 'Se ha conectado y desconectado el POS correctamente' });
-
-            } else {
-                res.status(200).json({ message: 'Solo se ha conseguido conectar al POS' });
-            }
+            
         }
 
     } catch (error) {
